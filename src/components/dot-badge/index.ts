64394aed63728878b80f7a77487de27aa2abdb5d
Camilo Vargas <ca06vargas@gmail.com>
2026-04-13 15:50:49 -0500

    chore: add .nvmrc for Node version 22.17.1, update package.json to include 'lit' dependency, and enhance DotBadge component with LitElement integration and property decorators

--- src/components/dot-badge/index.ts
+++ src/components/dot-badge/index.ts
@@ -3,8 +3,6 @@ import DotBadge from './dot-badge'
 export * from './dot-badge'
 export default DotBadge
 
-DotBadge.define('dot-badge')
-
 declare global {
     interface HTMLElementTagNameMap {
         'dot-badge': DotBadge
