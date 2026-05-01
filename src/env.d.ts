ddaa5dc13167d0b50fb0fbc8e9f6c2f5ecfd2ff4
Camilo Vargas <ca06vargas@gmail.com>
2026-04-30 21:54:21 -0500

    refactor: simplify project structure by removing tooling and development playground files

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '@fontsource-variable/dm-sans' {}
-declare module '@fontsource-variable/dm-sans/wght-italic.css' {}
-declare module '@fontsource/dm-mono/400.css' {}
-declare module '@fontsource/dm-mono/500.css' {}
